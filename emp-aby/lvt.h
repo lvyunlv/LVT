75b8f9adbb6b0ae10a542f470053ec64dbdf7ca1
Derrick-Wan <scuderrickwan@gmail.com>
2025-04-30 18:32:38 +0800

    lyl

--- emp-aby/lvt.h
+++ emp-aby/lvt.h
@@ -100,44 +100,45 @@ LVT<IO>::LVT(int num_party, int party, MPIOChannel<IO>* io, ThreadPool* pool, EL
 }
 
 void build_safe_P_to_m(std::map<std::string, Fr>& P_to_m, int num_party, size_t tb_size) {
-    size_t max_exponent = 2 * tb_size * num_party;
+    // size_t max_exponent = 2 * tb_size * num_party;
+    size_t max_exponent = 128;
     
     // // 如果表较小，直接计算
     // if (max_exponent <= 1<<8) {
-    //     // 测试时间
-    //     // auto start_time = chrono::high_resolution_clock::now();
-    //     cout << "开始构建大小为" << max_exponent << "的P_to_m表..." << endl;
-    //     for (size_t i = 0; i <= max_exponent; ++i) {
-    //         BLS12381Element g_i(i);
-    //         P_to_m[g_i.getPoint().getStr()] = Fr(to_string(i));
-    //     }
-    //     cout << "P_to_m表构建完成" << endl;
-    //     // auto end_time = chrono::high_resolution_clock::now();
-    //     // auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
-    //     // cout << "构建表用时: " << duration.count() << " 毫秒" << endl;
-    //     return;
-    // }
-    
-    // 如果表较大，尝试读取文件
-    // auto start_time = chrono::high_resolution_clock::now();
-    cout << "开始读取P_to_m表..." << endl;
-    const char* filename = "P_to_m_table.bin";
-    deserialize_P_to_m(P_to_m, filename);
-    cout << "P_to_m表读取完成" << endl;
-    // auto end_time = chrono::high_resolution_clock::now();
-    // auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
-    // cout << "读取表用时: " << duration.count() << " 毫秒" << endl;
-    
-    // 如果文件不存在或读取失败，则计算并保存
-    if (P_to_m.empty()) {
-        cout << "P_to_m表为空，开始计算..." << endl;
+        // 测试时间
+        // auto start_time = chrono::high_resolution_clock::now();
+        // cout << "开始构建大小为" << max_exponent << "的P_to_m表..." << endl;
         for (size_t i = 0; i <= max_exponent; ++i) {
             BLS12381Element g_i(i);
-            P_to_m[g_i.getPoint().getStr()] = Fr(i);
+            P_to_m[g_i.getPoint().getStr()] = Fr(to_string(i));
         }
-        serialize_P_to_m(P_to_m, filename);
-        cout << "P_to_m表计算完成" << endl;
-    }
+        // cout << "P_to_m表构建完成" << endl;
+        // auto end_time = chrono::high_resolution_clock::now();
+        // auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
+        // cout << "构建表用时: " << duration.count() << " 毫秒" << endl;
+        return;
+    // }
+    
+    // // 如果表较大，尝试读取文件
+    // // auto start_time = chrono::high_resolution_clock::now();
+    // cout << "开始读取P_to_m表..." << endl;
+    // const char* filename = "P_to_m_table.bin";
+    // deserialize_P_to_m(P_to_m, filename);
+    // cout << "P_to_m表读取完成" << endl;
+    // // auto end_time = chrono::high_resolution_clock::now();
+    // // auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
+    // // cout << "读取表用时: " << duration.count() << " 毫秒" << endl;
+    
+    // // 如果文件不存在或读取失败，则计算并保存
+    // if (P_to_m.empty()) {
+    //     cout << "P_to_m表为空，开始计算..." << endl;
+    //     for (size_t i = 0; i <= max_exponent; ++i) {
+    //         BLS12381Element g_i(i);
+    //         P_to_m[g_i.getPoint().getStr()] = Fr(i);
+    //     }
+    //     serialize_P_to_m(P_to_m, filename);
+    //     cout << "P_to_m表计算完成" << endl;
+    // }
 }
 
 template <typename IO>
@@ -148,28 +149,28 @@ LVT<IO>::LVT(int num_party, int party, MPIOChannel<IO>* io, ThreadPool* pool, EL
     // cout << "table size: " << tb_size << endl;
     if (tb_size <= 65536) build_safe_P_to_m(P_to_m, num_party, tb_size);
 
-    uint64_t N = 1ULL << 38; // 32-bit空间
-    try {
-        // auto start_time = chrono::high_resolution_clock::now();
-        std::cout << "开始加载bsgs表..." << std::endl;
-        bsgs.deserialize("bsgs_table.bin");
-        cout << "bsgs表加载完成" << endl;
+    // uint64_t N = 1ULL << 38; // 32-bit空间
+    // try {
+    //     // auto start_time = chrono::high_resolution_clock::now();
+    //     std::cout << "开始加载bsgs表..." << std::endl;
+    //     bsgs.deserialize("bsgs_table.bin");
+    //     cout << "bsgs表加载完成" << endl;
         
-        // auto end_time = chrono::high_resolution_clock::now();
-        // auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
-        // cout << "读取表用时: " << duration.count() << " 毫秒" << endl;
-        // std::cout << "成功加载预计算数据" << std::endl;
-    } catch (const std::exception& e) {
-        std::cout << "bsgs预计算数据不存在或损坏，开始预计算..." << std::endl;
-        // auto start_time = chrono::high_resolution_clock::now();
-        bsgs.precompute(g, N);
-        // auto end_time = chrono::high_resolution_clock::now();
-        // auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
-        // cout << "预计算用时: " << duration.count() << " 毫秒" << endl;
-        // std::cout << "预计算完成，保存到文件..." << std::endl;
-        bsgs.serialize("bsgs_table.bin");
-        cout << "bsgs表保存完成" << endl;
-    }
+    //     // auto end_time = chrono::high_resolution_clock::now();
+    //     // auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
+    //     // cout << "读取表用时: " << duration.count() << " 毫秒" << endl;
+    //     // std::cout << "成功加载预计算数据" << std::endl;
+    // } catch (const std::exception& e) {
+    //     std::cout << "bsgs预计算数据不存在或损坏，开始预计算..." << std::endl;
+    //     // auto start_time = chrono::high_resolution_clock::now();
+    //     bsgs.precompute(g, N);
+    //     // auto end_time = chrono::high_resolution_clock::now();
+    //     // auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
+    //     // cout << "预计算用时: " << duration.count() << " 毫秒" << endl;
+    //     // std::cout << "预计算完成，保存到文件..." << std::endl;
+    //     bsgs.serialize("bsgs_table.bin");
+    //     cout << "bsgs表保存完成" << endl;
+    // }
 }
 
 template <typename IO>
